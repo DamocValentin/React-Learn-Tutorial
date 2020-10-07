ea8459c5c66168f7765b12ce9d16c639e681737f
DamocValentin <valentin.damoc@yahoo.com>
2020-10-07 11:44:44 +0300

    adding counters component

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,21 +2,26 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
   };
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.handleIncrement = this.handleIncrement.bind(this);
+    this.handleDecrease = this.handleDecrease.bind(this);
   }
 
   handleIncrement() {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
+  }
+
+  handleDecrease() {
+    this.setState({ value: this.state.value > 0 ? this.state.value - 1 : 0 });
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
@@ -34,6 +39,12 @@ class Counter extends Component {
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={this.handleDecrease}
+          className="btn btn-secondary btn-sm"
+        >
+          Decrease
         </button>
       </div>
     );
